705194a9c8a5b2cfd9b6922cbedad3f02537491b
azmir849 <m.azmir.cse@gmail.com>
2022-06-06 18:00:57 +0600

    magazine sliders done

--- index.php
+++ index.php
@@ -6,27 +6,9 @@
             <div class="home_wrapper">
                 <div class="row">
                     <div class="col-lg-3">
-                         <div class="sideBar">
-                             <div class="sideBar_wrapper">
-                                   <ul>
-                                       <li>
-                                           <div class="name"><a href="#">নামের তথ্য</a></div>
-                                           <div class="edit">
-                                               <ul>
-                                                   <li><i class="fa-solid fa-pen-to-square"></i></li>
-                                                   <li><i class="fa-solid fa-trash"></i></li>
-                                               </ul>
-                                           </div>
-                                       </li>
-                                       <li>
-                                        <div class="name"><a href="#">নামের তথ্য</a></div>
-                                        <div class="edit">
-                                            <ul>
-                                                <li><i class="fa-solid fa-pen-to-square"></i></li>
-                                                <li><i class="fa-solid fa-trash"></i></li>
-                                            </ul>
-                                        </div>
-                                    </li>
+                        <div class="sideBar">
+                            <div class="sideBar_wrapper">
+                                <ul>
                                     <li>
                                         <div class="name"><a href="#">নামের তথ্য</a></div>
                                         <div class="edit">
@@ -99,25 +81,44 @@
                                             </ul>
                                         </div>
                                     </li>
-                                   </ul>
-                             </div>
-                         </div>
+                                    <li>
+                                        <div class="name"><a href="#">নামের তথ্য</a></div>
+                                        <div class="edit">
+                                            <ul>
+                                                <li><i class="fa-solid fa-pen-to-square"></i></li>
+                                                <li><i class="fa-solid fa-trash"></i></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="name"><a href="#">নামের তথ্য</a></div>
+                                        <div class="edit">
+                                            <ul>
+                                                <li><i class="fa-solid fa-pen-to-square"></i></li>
+                                                <li><i class="fa-solid fa-trash"></i></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-lg-9">
                         <div class="banner">
-                            <div class="banner_item">
-                                <img src="images/banner1.png" alt="">
-                            </div>
-                            <div class="banner_item">
-                                <img src="images/banner2.png" alt="">
-                            </div>
+                            <?php if (!empty($sliders)) { ?>
+                                <?php foreach ($sliders as $key => $slide) { ?>
+                                    <div class="banner_item">
+                                        <img src="<?= "https://icircles.app/uploads/slider/" . $ms_id . "/" . $slide->image ?>" alt="">
+                                    </div>
+                                <?php } ?>
+                            <?php } ?>
                         </div>
 
                         <div class="profiledesk">
                             <div class="profile">
-                                <h5>  সম্পাদকের ডেস্ক</h5>
+                                <h5> সম্পাদকের ডেস্ক</h5>
                                 <div class="profileImg">
-                                       <img src="images/poetry.jpg" alt="">
+                                    <img src="images/poetry.jpg" alt="">
                                 </div>
                                 <div class="profileBio">
                                     <p>গোলাম মোস্তফা অনন্ত অসীম প্রেমময় তুমি বিচার দিনের স্বামী। যত গুণগান হে চির মহান তোমারি অন্তর্যামী। দ্যুলোক-ভূলোক সবারে ছাড়িয়া তোমারি চরণে পড়ি লুটাইয়া তোমারি সকাশে যাচি হে শকতি তোমারি করুণাকামী। সরল সঠিক পূণ্য পন্থা মোদের দাও গো বলি, চালাও সে-পথে যে-পথে তোমার প্রিয়জন গেছে চলি। যে-পথে তোমার চির-অভিশাপ যে-পথে ভ্রান্তি, চির-পরিতাপ হে মহাচালক,মোদের কখনও করো না সে পথগামী। </p>
@@ -140,4 +141,3 @@
 <!--------------------- Home Section End --------------------------------------------------->
 
 <?php include("./include/footer.php") ?>
-
